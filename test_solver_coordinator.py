60edf554bb28a44e041c3880ed33bb2bf73de620
Engineer <jasonmrimer@gmail.com>
2021-10-03 22:40:19 -0700

    works for randomly generated values m=2 n=4

--- test_solver_coordinator.py
+++ test_solver_coordinator.py
@@ -59,17 +59,50 @@ class MyTestCase(unittest.TestCase):
         )
 
     def test_main_random(self):
-        expected_precision_decimal_places = 6
-        constraint_generator = ConstraintGenerator(20)
+        expected_precision_decimal_places = 3
+        constraint_generator = ConstraintGenerator(26)
 
         minimizer = constraint_generator.generate_minimizer(4)
+        constraint_coefficients = constraint_generator.generate_random_constraints(2, 4)
+        constraint_resolutions = constraint_generator.generate_constraint_resolutions(2)
 
-        constraint_coefficients = constraint_generator.generate_random_constraints(2,4)
+        solver = solver_coordinator.main(
+            minimizer,
+            constraint_coefficients,
+            constraint_resolutions
+        )
 
-        constraint_resolutions = [
-            1,
-            1
-        ]
+        self.assertAlmostEqual(
+            -5.2857,
+            solver.Objective().Value(),
+            expected_precision_decimal_places,
+            'optimal value not equal'
+        )
+
+        self.assertAlmostEqual(
+            0.571,
+            solver.LookupVariable('x1').solution_value(),
+            expected_precision_decimal_places,
+            'x1 value not within expected range'
+        )
+        self.assertAlmostEqual(
+            0.0,
+            solver.LookupVariable('x2').solution_value(),
+            expected_precision_decimal_places,
+            'x2 value not within expected range'
+        )
+        self.assertAlmostEqual(
+            0.4285,
+            solver.LookupVariable('x3').solution_value(),
+            expected_precision_decimal_places,
+            'x3 value not within expected range'
+        )
+        self.assertAlmostEqual(
+            0.0,
+            solver.LookupVariable('x4').solution_value(),
+            expected_precision_decimal_places,
+            'x4 value not within expected range'
+        )
 
 
 if __name__ == '__main__':
