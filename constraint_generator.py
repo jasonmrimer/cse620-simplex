8720620094ff69b58433a5e1e787aa2d0bb547a5
Engineer <jasonmrimer@gmail.com>
2021-10-03 22:59:17 -0700

    Functioning optimizer with increasing n. Outputs to console for easy parsing by Google Sheets.

--- constraint_generator.py
+++ constraint_generator.py
@@ -6,24 +6,60 @@ class ConstraintGenerator(object):
     def __init__(self, seed):
         self.seed = seed
 
-    def random_in_range(self, multiplier):
+    def random_in_range(self, multiplier, range_min, range_max):
         random.seed(self.seed * multiplier)
-        return random.randint(-10, 10)
+        return random.randint(range_min, range_max)
 
     def generate_random_coefficients(self, variable_count, multiplier):
-        variables = []
-        for x in range(variable_count):
-            variables.append(self.random_in_range((x + 1) * multiplier))
-        return variables
+        def multiplier_func(index):
+            return multiplier * (index + 1)
+
+        return self.looping_randomizer(
+            multiplier_func,
+            variable_count,
+            -10,
+            10
+        )
 
     def generate_random_constraints(self, constraint_count, variable_count):
         constraints = []
         for x in range(constraint_count):
-            constraints.append(self.generate_random_coefficients(variable_count, x + 1))
+            constraints.append(
+                self.generate_random_coefficients(variable_count, x + 1)
+            )
         return constraints
 
-    def generate_minimizer(self, variable_count):
-        return self.generate_random_coefficients(variable_count, self.seed)
+    def generate_minimizer(self, constraint_count, variable_count):
+        def multiplier_func(index):
+            return (constraint_count + 1) * variable_count * (index + 1)
+
+        return self.looping_randomizer(multiplier_func, variable_count, 0, 10)
 
     def generate_constraint_resolutions(self, constraint_count):
-        return self.generate_random_coefficients(constraint_count, math.sqrt(self.seed))
+        seed = self.seed
+
+        def multiplier_func(index):
+            return math.sqrt(seed) * index
+
+        return self.looping_randomizer(
+            multiplier_func,
+            constraint_count,
+            0,
+            100
+        )
+
+    def looping_randomizer(
+            self,
+            multiplier_func,
+            variable_count,
+            range_min,
+            range_max
+    ):
+        coefficients = []
+        for x in range(variable_count):
+            coefficients.append(
+                self.random_in_range(
+                    multiplier_func(x), range_min, range_max
+                )
+            )
+        return coefficients
---
+++ fixed_m_increase_n.py
@@ -0,0 +1,110 @@
+import unittest
+
+import solver_coordinator
+from constraint_generator import ConstraintGenerator
+
+
+def feasible_solver_seed_finder(seed, solver, constraint_count, variable_count):
+    while (True):
+        constraint_generator = ConstraintGenerator(seed)
+
+        minimizer = constraint_generator.generate_minimizer(constraint_count, variable_count)
+        constraint_coefficients = constraint_generator.generate_random_constraints(constraint_count, variable_count)
+        constraint_resolutions = constraint_generator.generate_constraint_resolutions(constraint_count)
+
+        solver = solver_coordinator.main(
+            minimizer,
+            constraint_coefficients,
+            constraint_resolutions
+        )
+
+        seed += 1
+
+        if solver:
+            break
+
+    print(f'found seed: {seed}')
+    return solver
+
+
+def run_solution(constraint_generator, solver, variable_count, constraint_count):
+    minimizer = constraint_generator.generate_minimizer(constraint_count, variable_count)
+    constraint_coefficients = constraint_generator.generate_random_constraints(constraint_count, variable_count)
+    constraint_resolutions = constraint_generator.generate_constraint_resolutions(constraint_count)
+    solver = solver_coordinator.main(
+        minimizer,
+        constraint_coefficients,
+        constraint_resolutions
+    )
+    return solver
+
+
+class MyTestCase(unittest.TestCase):
+    seed = 1
+
+    def test_n_10(self):
+        print('\nstart n10')
+        constraint_count = 2
+        variable_count = 10
+        constraint_generator = ConstraintGenerator(self.seed)
+
+        solver = None
+        # feasible_solver_seed_finder(self.seed, solver, constraint_count, variable_count)
+
+        solver = run_solution(constraint_generator, solver, variable_count, constraint_count)
+
+        self.assertIsNotNone(solver.Objective().Value())
+        print(f'optimal solution: {solver.Objective().Value()}')
+
+
+    def test_n_20(self):
+        print('\nstart n20')
+        constraint_count = 2
+        variable_count = 20
+        constraint_generator = ConstraintGenerator(self.seed)
+
+        solver = None
+        solver = run_solution(constraint_generator, solver, variable_count, constraint_count)
+
+        self.assertIsNotNone(solver.Objective().Value())
+        print(f'optimal solution: {solver.Objective().Value()}')
+
+    def test_n_30(self):
+        print('\nstart n30')
+        constraint_count = 2
+        variable_count = 30
+        constraint_generator = ConstraintGenerator(self.seed)
+
+        solver = None
+        solver = run_solution(constraint_generator, solver, variable_count, constraint_count)
+
+        self.assertIsNotNone(solver.Objective().Value())
+        print(f'optimal solution: {solver.Objective().Value()}')
+
+    def test_n_40(self):
+        print('\nstart n40')
+        constraint_count = 2
+        variable_count = 40
+        constraint_generator = ConstraintGenerator(self.seed)
+
+        solver = None
+        solver = run_solution(constraint_generator, solver, variable_count, constraint_count)
+
+        self.assertIsNotNone(solver.Objective().Value())
+        print(f'optimal solution: {solver.Objective().Value()}')
+
+    def test_n_50(self):
+        print('\nstart n50')
+        constraint_count = 2
+        variable_count = 50
+        constraint_generator = ConstraintGenerator(self.seed)
+
+        solver = None
+        solver = run_solution(constraint_generator, solver, variable_count, constraint_count)
+
+        self.assertIsNotNone(solver.Objective().Value())
+        print(f'optimal solution: {solver.Objective().Value()}')
+
+
+if __name__ == '__main__':
+    unittest.main()
