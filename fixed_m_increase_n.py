2cae71ce2ecd90304a2ef27088a39d7eb56f0caf
Engineer <jasonmrimer@gmail.com>
2021-10-04 19:32:29 -0700

    a little refactor for the solver_coordinator. moved setup out of tests and into domain of coordinator; moved variable, minimizer, and coefficient setup to separate methods to improve readablility and responsbility

--- fixed_m_increase_n.py
+++ fixed_m_increase_n.py
@@ -1,66 +1,6 @@
 import unittest
 
 import solver_coordinator
-from constraint_generator import ConstraintGenerator
-
-
-def feasible_solver_seed_finder(
-        seed,
-        constraint_count,
-        variable_count
-):
-    while (True):
-        constraint_generator = ConstraintGenerator(seed)
-
-        minimizer = constraint_generator.generate_minimizer(
-            constraint_count,
-            variable_count
-        )
-        constraint_coefficients = constraint_generator.generate_random_constraints(
-            constraint_count,
-            variable_count
-        )
-        constraint_resolutions = constraint_generator.generate_constraint_resolutions(
-            constraint_count
-        )
-
-        solver = solver_coordinator.main(
-            minimizer,
-            constraint_coefficients,
-            constraint_resolutions
-        )
-
-        seed += 1
-
-        if solver:
-            break
-
-    print(f'found seed: {seed}')
-    return solver
-
-
-def run_solution(
-        constraint_generator,
-        variable_count,
-        constraint_count
-):
-    minimizer = constraint_generator.generate_minimizer(
-        constraint_count,
-        variable_count
-    )
-    constraint_coefficients = constraint_generator.generate_random_constraints(
-        constraint_count,
-        variable_count
-    )
-    constraint_resolutions = constraint_generator.generate_constraint_resolutions(
-        constraint_count
-    )
-    solver = solver_coordinator.main(
-        minimizer,
-        constraint_coefficients,
-        constraint_resolutions
-    )
-    return solver
 
 
 def print_solution(n, solver):
@@ -70,91 +10,20 @@ def print_solution(n, solver):
 
 class MyTestCase(unittest.TestCase):
     seed = 1
+    constraint_count = 2
 
     def test_n_increasing(self):
         for n in range(10, 60, 10):
-            constraint_generator = ConstraintGenerator(self.seed)
-            constraint_count = 2
             variable_count = n
-            solver = run_solution(
-                constraint_generator,
+            solver = solver_coordinator.main_refactored(
+                self.seed,
+                self.constraint_count,
                 variable_count,
-                constraint_count
+                False
             )
+
             self.assertIsNotNone(solver.Objective().Value())
             print_solution(n, solver)
-
-    @unittest.skip
-    def test_n_10(self):
-        print('\nstart n10')
-        constraint_count = 2
-        variable_count = 10
-        constraint_generator = ConstraintGenerator(self.seed)
-
-        solver = None
-        # feasible_solver_seed_finder(self.seed, solver, constraint_count, variable_count)
-
-        solver = run_solution(constraint_generator, variable_count,
-                              constraint_count)
-
-        self.assertIsNotNone(solver.Objective().Value())
-        print_solution(10, solver)
-
-    @unittest.skip
-    def test_n_20(self):
-        print('\nstart n20')
-        constraint_count = 2
-        variable_count = 20
-        constraint_generator = ConstraintGenerator(self.seed)
-
-        solver = None
-        solver = run_solution(constraint_generator, variable_count,
-                              constraint_count)
-
-        self.assertIsNotNone(solver.Objective().Value())
-        print_solution(20, solver)
-
-    @unittest.skip
-    def test_n_30(self):
-        print('\nstart n30')
-        constraint_count = 2
-        variable_count = 30
-        constraint_generator = ConstraintGenerator(self.seed)
-
-        solver = None
-        solver = run_solution(constraint_generator, variable_count,
-                              constraint_count)
-
-        self.assertIsNotNone(solver.Objective().Value())
-        print_solution(30, solver)
-
-    @unittest.skip
-    def test_n_40(self):
-        print('\nstart n40')
-        constraint_count = 2
-        variable_count = 40
-        constraint_generator = ConstraintGenerator(self.seed)
-
-        solver = None
-        solver = run_solution(constraint_generator, variable_count,
-                              constraint_count)
-
-        self.assertIsNotNone(solver.Objective().Value())
-        print_solution(40, solver)
-
-    @unittest.skip
-    def test_n_50(self):
-        print('\nstart n50')
-        constraint_count = 2
-        variable_count = 50
-        constraint_generator = ConstraintGenerator(self.seed)
-
-        solver = None
-        solver = run_solution(constraint_generator, variable_count,
-                              constraint_count)
-
-        self.assertIsNotNone(solver.Objective().Value())
-        print_solution(50, solver)
 
 
 if __name__ == '__main__':
--- solver_coordinator.py
+++ solver_coordinator.py
@@ -1,5 +1,40 @@
 from ortools.linear_solver import pywraplp
 
+from constraint_generator import ConstraintGenerator
+
+
+def main_refactored(
+        seed,
+        constraint_count,
+        variable_count,
+        print_results=False
+):
+    constraint_generator = ConstraintGenerator(seed)
+
+    minimizer = constraint_generator.generate_minimizer(
+        constraint_count,
+        variable_count
+    )
+    constraint_coefficients = constraint_generator.generate_random_constraints(
+        constraint_count,
+        variable_count
+    )
+    constraint_resolutions = constraint_generator.generate_constraint_resolutions(
+        constraint_count
+    )
+    solver = main(
+        minimizer,
+        constraint_coefficients,
+        constraint_resolutions,
+        print_results
+    )
+
+    return solver
+
+
+def no_solution(status):
+    return status != pywraplp.Solver.OPTIMAL
+
 
 def main(
         minimizer,
@@ -9,32 +44,20 @@ def main(
 ):
     solver = pywraplp.Solver.CreateSolver('GLOP')
 
-    variables = []
-    for i in range(len(constraint_coefficients[0])):
-        variables.append(solver.NumVar(0, solver.infinity(), f'x{i + 1}'))
+    variables = set_variables(constraint_coefficients, solver)
 
-    constraint_list = []
-    for i in range(len(constraint_resolutions)):
-        constraint_list.append(
-            solver.Constraint(
-                constraint_resolutions[i],
-                constraint_resolutions[i]
-            )
-        )
-        for j in range(len(variables)):
-            constraint_list[i].SetCoefficient(
-                variables[j],
-                constraint_coefficients[i][j]
-            )
+    set_constraint_equations(
+        solver,
+        constraint_resolutions,
+        constraint_coefficients,
+        variables
+    )
 
-    objective = solver.Objective()
-    for i in range(len(variables)):
-        objective.SetCoefficient(variables[i], minimizer[i])
-
-    objective.SetMinimization()
+    objective = setup_minimizer(solver, minimizer, variables)
 
     status = solver.Solve()
-    if status != pywraplp.Solver.OPTIMAL:
+
+    if no_solution(status):
         print('The problem does not have an optimal solution.')
         return None
 
@@ -50,13 +73,50 @@ def main(
     return solver
 
 
-def print_constraints(constraint_resolutions, constraint_coefficients,
-                      variables):
+def setup_minimizer(solver, minimizer, variables):
+    objective = solver.Objective()
+    for i in range(len(variables)):
+        objective.SetCoefficient(variables[i], minimizer[i])
+    objective.SetMinimization()
+    return objective
+
+
+def set_constraint_equations(solver, constraint_resolutions,
+                             constraint_coefficients, variables):
+    constraint_list = []
+    for i in range(len(constraint_resolutions)):
+        constraint_list.append(
+            solver.Constraint(
+                constraint_resolutions[i],
+                constraint_resolutions[i]
+            )
+        )
+        for j in range(len(variables)):
+            constraint_list[i].SetCoefficient(
+                variables[j],
+                constraint_coefficients[i][j]
+            )
+
+
+def set_variables(constraint_coefficients, solver):
+    variables = []
+    for i in range(len(constraint_coefficients[0])):
+        variables.append(solver.NumVar(0, solver.infinity(), f'x{i + 1}'))
+    return variables
+
+
+def print_constraints(
+        constraint_resolutions,
+        constraint_coefficients,
+        variables
+):
     for i in range(len(constraint_resolutions)):
         print(f'constraint {i + 1} res: {constraint_resolutions[i]}')
         for j in range(len(variables)):
             print(
-                f'coefficient: {variables[j].name()}: {constraint_coefficients[i][j]}')
+                f'coefficient: {variables[j].name()}: '
+                f'{constraint_coefficients[i][j]}'
+            )
 
 
 def print_minimizer(variables, objective):
