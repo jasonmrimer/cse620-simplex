b0bebabf6924700cb99d5c0347f19c9ebc217ffd
Engineer <jasonmrimer@gmail.com>
2021-10-05 22:23:20 -0700

    Working code for running, solving, and printing all requirements in assignment

--- increase_m_increase_n.py
+++ increase_m_increase_n.py
@@ -9,18 +9,15 @@ class SolutionSeed:
         self.seed = seed
 
 
-def feasible_solver_seed_finder(
-        seed,
-        constraint_count,
-        variable_count
-):
+def feasible_solver_seed_finder(seed, constraint_count, variable_count,
+                                should_print_results):
     while True:
         # print('whilestart')
         solution = solver_coordinator.main_refactored(
             seed,
             constraint_count,
             variable_count,
-
+            should_print_results
         )
         # print(f'objective: {solution}')
         if is_feasible_solution(solution):
@@ -39,11 +36,16 @@ class MyTestCase(unittest.TestCase):
     print_details = False
 
     def test_m_increasing_n_increasing(self):
+        should_print_results = True
         for constraint_count in range(2, 15, 4):
-            self.run_solver_with_configs(constraint_count, 4, False)
+            self.run_solver_with_configs(constraint_count, 4,
+                                         should_print_results)
             for variable_count in range(10, 51, 10):
-                self.run_solver_with_configs(constraint_count, variable_count,
-                                             False)
+                self.run_solver_with_configs(
+                    constraint_count,
+                    variable_count,
+                    should_print_results
+                )
 
     def test_multiple_times(self):
         constraint_count = 2
@@ -60,14 +62,11 @@ class MyTestCase(unittest.TestCase):
                                 should_print_results):
         # print(f'm = {constraint_count} | n = {variable_count}')
         seed = 200
-        solutionSeed = feasible_solver_seed_finder(
-            seed,
-            constraint_count,
-            variable_count
-        )
+        solutionSeed = feasible_solver_seed_finder(seed, constraint_count,
+                                                   variable_count, True)
         # print(f'wall time: {solutionSeed.solution.wall_time}')
-        print(f'seed {solutionSeed.seed}')
-        print(f'{solutionSeed.solution.wall_time}')
+        # print(f'seed {solutionSeed.seed}')
+        # print(f'{solutionSeed.solution.wall_time}')
         # self.assertIsNotNone(solver.Objective().Value())
         # print_solution(
         #     constraint_count,
